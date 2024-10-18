a39d5e280b6a24a74bdb4ef21cf8784a0fcef38b
thomas.galinier <thomas.galinier@braincube.com>
2024-10-18 10:48:09 +0200

    put

--- index.js
+++ index.js
@@ -66,8 +66,32 @@ app.delete('/todos/:id', (req, res) => {
         });
     });
 });
+app.put('/todos/:id', (req, res) => {
+    const todoId = parseInt(req.params.id);
+    const updatedTodo = req.body;
 
+    fs.readFile(TODO_FILE, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Erreur de lecture du fichier' });
+        }
 
+        let todos = JSON.parse(data);
+        const todoIndex = todos.findIndex(todo => todo.id === todoId);
+
+        if (todoIndex === -1) {
+            return res.status(404).json({ message: 'Tâche non trouvée' });
+        }
+
+        todos[todoIndex] = { ...todos[todoIndex], ...updatedTodo };
+
+        fs.writeFile(TODO_FILE, JSON.stringify(todos, null, 2), 'utf8', (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Erreur lors de l\'écriture dans le fichier' });
+            }
+            res.status(200).json(todos[todoIndex]);
+        });
+    });
+});
 app.listen(port, () => {
     console.log(`Serveur en écoute sur http://localhost:${port}`);
 });
